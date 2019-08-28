3bd7465df983e7bd605b36e80d1d17b77c19051d
wuchuheng <root@wuchuheng.com>
2019-08-28 15:42:24 +0000

    update

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '2831473954@qq.com'),
+        'name' => env('MAIL_FROM_NAME', '2831473954@qq.com'),
     ],
 
     /*
